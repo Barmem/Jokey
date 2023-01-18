3ac17afe138f5ab8a06e622291fa86ae6dc1fef8
barmem <igor.neim7@gmail.com>
2023-01-18 16:32:10 +0600

    created passRequestConvo, moved parser outside of main

--- convo.py
+++ convo.py
@@ -1,55 +1,118 @@
 from telegram import ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import Updater, CommandHandler, ConversationHandler, MessageHandler, Filters
+import main
+from sqlaccess import get_records, insert_records
+import re
+import time
 
 # Define the states for the conversation
-SELECT_DESTINATION, CONFIRM_DESTINATION = range(2)
+CAR_INPUT, HUMAN_INPUT = range(2)
 
-def fly(update, context):
-    """Start the conversation to select a destination to fly to."""
-    update.message.reply_text('Where do you want to fly?',)
-    return SELECT_DESTINATION
-
-def select_destination(update, context):
-    """Store the selected destination."""
-    user = update.message.from_user
-    context.user_data['destination'] = update.message.text
-    # button1 = InlineKeyboardButton("Да", callback_data="Yes")
-    # button2 = InlineKeyboardButton("Нет", callback_data="No")
-    keyboard = ReplyKeyboardMarkup([["Yes"], ["no"]], one_time_keyboard=True)
-    # keyboard = InlineKeyboardMarkup([[button1, button2]])
-    update.message.reply_text('Do you want to fly to {}?'.format(update.message.text),
-                              reply_markup=keyboard)
-    return CONFIRM_DESTINATION
-
-def confirm_destination(update, context):
-    """Confirm the selected destination."""
-    user = update.message.from_user
-    if update.message.text == 'Yes':
-        update.message.reply_text('Great, we will fly to {}!'.format(context.user_data['destination']),
-                              reply_markup=ReplyKeyboardRemove())
-        return ConversationHandler.END
-    else:
-        update.message.reply_text('Okay, let\'s try again.',
-                              reply_markup=ReplyKeyboardRemove())
-        return SELECT_DESTINATION
-
-def cancel(update, context):
-    """End the conversation."""
-    user = update.message.from_user
-    update.message.reply_text('Bye!',
-                              reply_markup=ReplyKeyboardRemove())
-    return ConversationHandler.END
-
-def convoStart():
+def passRequestConvo():
     # Define the conversation handler
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler('fly', fly)],
+        entry_points=[CommandHandler(['car','human'], isreg)],
 
         states={
-            SELECT_DESTINATION: [MessageHandler(Filters.text, select_destination)],
-            CONFIRM_DESTINATION: [MessageHandler(Filters.text, confirm_destination)]
+            CAR_INPUT: [MessageHandler(Filters.text, car_input)],
+            HUMAN_INPUT: [MessageHandler(Filters.text, human_input)]
         },
 
         fallbacks=[CommandHandler('cancel', cancel)]
     )
     return conv_handler
+
+
+def isreg(update, context):
+    """Start the conversation."""
+    records = get_records(
+        main.args.host,
+        main.args.user,
+        main.args.password,
+        main.args.database,
+        f"SELECT * FROM People WHERE `Telegram ID`= {update.effective_user.id}",
+        )
+    # print(records[0][9])
+
+    command = update.message.text.split()[0][1:]
+
+    if(len(records) != 0):
+        if(command == "car"):
+            update.message.reply_text('Введите ПОЛНЫЙ номер авто.')
+            return CAR_INPUT
+            # return ConversationHandler.END
+        elif(command == "human"):
+            update.message.reply_text('Введите полный ФИО гостя.')
+            return HUMAN_INPUT
+            # return ConversationHandler.END
+    else:
+        update.message.reply_text('Вы не являетесь зарегестрированным пользователем.')
+        return ConversationHandler.END
+
+def car_input(update, context):
+    myregex = re.compile(r"^(([АВЕКМНОРСТУХ]\d{3}(?<!000)[АВЕКМНОРСТУХ]{1,2})(\d{2,3})|(\d{4}(?<!0000)[АВЕКМНОРСТУХ]{2})(\d{2})|(\d{3}(?<!000)(C?D|[ТНМВКЕ])\d{3}(?<!000))(\d{2}(?<!00))|([ТСК][АВЕКМНОРСТУХ]{2}\d{3}(?<!000))(\d{2})|([АВЕКМНОРСТУХ]{2}\d{3}(?<!000)[АВЕКМНОРСТУХ])(\d{2})|([АВЕКМНОРСТУХ]\d{4}(?<!0000))(\d{2})|(\d{3}(?<!000)[АВЕКМНОРСТУХ])(\d{2})|(\d{4}(?<!0000)[АВЕКМНОРСТУХ])(\d{2})|([АВЕКМНОРСТУХ]{2}\d{4}(?<!0000))(\d{2})|([АВЕКМНОРСТУХ]{2}\d{3}(?<!000))(\d{2,3})|(^Т[АВЕКМНОРСТУХ]{2}\d{3}(?<!000)\d{2,3}))")
+    # А123ВЕ77
+    if myregex.match(update.message.text):
+        current_timestamp = round(time.time())
+
+        insert_records(
+            main.args.host,
+            main.args.user,
+            main.args.password,
+            main.args.database,
+            f"INSERT INTO Passes (`Car Number`, `People ID`, `Request Time`, `Expiration Time`, `Access ID`) VALUES ('{update.message.text}', (select `People ID` from People where `Telegram ID`='{update.effective_user.id}'), FROM_UNIXTIME({current_timestamp}), FROM_UNIXTIME({current_timestamp + (60*60*24)}), 1)",
+        )
+        update.message.reply_text('Пропуск авто оформлен.')
+        return ConversationHandler.END
+    else:
+        update.message.reply_text('Неверный номер авто.')
+        return ConversationHandler.END
+    
+    
+    # user = update.message.from_user
+    # context.user_data['destination'] = update.message.text
+    # # button1 = InlineKeyboardButton("Да", callback_data="Yes")
+    # # button2 = InlineKeyboardButton("Нет", callback_data="No")
+    # keyboard = ReplyKeyboardMarkup([["Yes"], ["no"]], one_time_keyboard=True)
+    # # keyboard = InlineKeyboardMarkup([[button1, button2]])
+    # update.message.reply_text('Do you want to fly to {}?'.format(update.message.text),
+    #                           reply_markup=keyboard)
+    # return CONFIRM_DESTINATION
+    
+
+def human_input(update, context):
+    current_timestamp = round(time.time())
+    myregex = re.compile(r"[^А-Яа-яЁё\s]+")    
+    result_string = re.sub(myregex, "", update.message.text)
+    if(result_string != ''):
+        insert_records(
+                main.args.host,
+                main.args.user,
+                main.args.password,
+                main.args.database,
+                f"INSERT INTO Passes (`Surname`, `People ID`, `Request Time`, `Expiration Time`, `Access ID`) VALUES ('{result_string}', (select `People ID` from People where `Telegram ID`='{update.effective_user.id}'), FROM_UNIXTIME({current_timestamp}), FROM_UNIXTIME({current_timestamp + (60*60*24)}), 2)",
+            )
+        update.message.reply_text(f"Пропуск на {result_string} оформлен")
+    else:
+        update.message.reply_text(f"Неверный формат ФИО.")
+    return ConversationHandler.END
+
+# def confirm_destination(update, context):
+#     """Confirm the selected destination."""
+#     user = update.message.from_user
+#     if update.message.text == 'Yes':
+#         update.message.reply_text('Great, we will fly to {}!'.format(context.user_data['destination']),
+#                               reply_markup=ReplyKeyboardRemove())
+#         return ConversationHandler.END
+#     else:
+#         update.message.reply_text('Okay, let\'s try again.',
+#                               reply_markup=ReplyKeyboardRemove())
+#         return SELECT_DESTINATION
+
+def cancel(update, context):
+    """End the conversation."""
+    # user = update.message.from_user
+    # update.message.reply_text('Bye!',
+    #                           reply_markup=ReplyKeyboardRemove())
+    return ConversationHandler.END
+
--- main.py
+++ main.py
@@ -11,6 +11,14 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 logger = logging.getLogger(__name__)
 
+# Parse command line arguments
+parser = argparse.ArgumentParser(description='Телеграм бот для обработки запросов проезда на территорию')
+parser.add_argument('token', help='Токен бота')
+parser.add_argument('--host', help='Хост БД')
+parser.add_argument('--user', help='Имя пользователя БД')
+parser.add_argument('--password', help='пароль БД')
+parser.add_argument('--database', help='Имя БД')
+args = parser.parse_args()
 
 def start(update, context):
     """Send a message when the command /start is issued."""
@@ -34,46 +42,39 @@ def sql(update, context, host, user, password, database,  param = ""):
 
 def error(update, context):
     """Log Errors caused by Updates."""
-    logger.warning('Update "%s" caused error "%s"', update, context.error)
+    logger.warning('error "%s"',  context.error)
 
 def main():
-    # Parse command line arguments
-    parser = argparse.ArgumentParser(description='Телеграм бот для обработки запросов проезда на территорию')
-    parser.add_argument('token', help='Токен бота')
-    parser.add_argument('--host', help='Хост БД')
-    parser.add_argument('--user', help='Имя пользователя БД')
-    parser.add_argument('--password', help='пароль БД')
-    parser.add_argument('--database', help='Имя БД')
-    args = parser.parse_args()
+
 
     # Create the Updater and pass it your bot's token.
     # Make sure to set use_context=True to use the new context based callbacks
     # Post version 12 this will no longer be necessary
-    updater = Updater(args.token, use_context=True)
+    updaterUser = Updater(args.token, use_context=True)
 
     # Get the dispatcher to register handlers
-    dp = updater.dispatcher
+    dp = updaterUser.dispatcher
 
-    conv_handler = convo.convoStart()
+    passRequestConvo_handler = convo.passRequestConvo()
 
     # Add the conversation handler to the dispatcher
-    dp.add_handler(conv_handler)
+    dp.add_handler(passRequestConvo_handler)
 
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-    dp.add_handler(CommandHandler("sql", lambda update, context: sql(update, context, args.host, args.user, args.password, args.database) ))
+    # dp.add_handler(CommandHandler("sql", lambda update, context: sql(update, context, args.host, args.user, args.password, args.database) ))
 
     # log all errors
     dp.add_error_handler(error)
 
     # Start the Bot
-    updater.start_polling()
+    updaterUser.start_polling()
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
     # start_polling() is non-blocking and will stop the bot gracefully.
-    updater.idle()
+    updaterUser.idle()
 
 if __name__ == '__main__':
     main()
